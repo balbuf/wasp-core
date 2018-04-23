cb5dc2b3ccaf18e749f39e6f5658e49c59ee0790
Stephen Beemsterboer <balbuf@gmail.com>
2018-04-23 11:23:47 -0400

    Add labels for image sizes

--- src/Handlers/ImageSizes.php
+++ src/Handlers/ImageSizes.php
@@ -18,11 +18,18 @@ class ImageSizes extends DependentHandler {
 	}
 
 	public function handle($transformer, $config, $property) {
+		$labels = [];
+
 		foreach ($config as $settings) {
 			// we must have both a width and height!
 			if (!isset($settings['width'], $settings['height'])) {
 				$this->logger->warning("Error: missing width or height for image size '{$settings['name']}'");
 				continue;
+			}
+
+			// is label is provided?
+			if (!empty($settings['label'])) {
+				$labels[$settings['name']] = $settings['label'];
 			}
 
 			// assemble function args
@@ -34,6 +41,30 @@ class ImageSizes extends DependentHandler {
 					'args' => $args,
 				]),
 				['hook' => 'after_setup_theme']
+			);
+		}
+
+		if (count($labels)) {
+			$transformer->outputExpression->addExpression(
+				$transformer->create('HookExpression', [
+					'name' => 'image_size_names_choose',
+					'expressions' => [
+						$transformer->create('CompositeExpression', [
+							'expressions' => [
+								$transformer->create('RawExpression', [
+									'expression' => 'return ',
+								]),
+								array_map(function($label) use ($transformer) {
+									return $transformer->create('TranslatableTextExpression', ['text' => $label]);
+								}, $labels),
+								$transformer->create('RawExpression', [
+									'expression' => ";\n",
+								]),
+							],
+						]),
+					],
+					'function' => 'add_filter',
+				])
 			);
 		}
 	}
